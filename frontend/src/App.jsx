9fbf91ededfebcb7b998275f176fe842a4304dd9
darshangodase <darshangodase10@gmail.com>
2024-08-23 12:00:38 +0530

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {BrowserRouter ,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUP from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+
 function App() {
   
 
   return (
-    <>
-      <h1 className=""></h1>
-    </>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUP />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+       </BrowserRouter>
   )
 } 
 
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>
+      <h1 className="">this is signin</h1>
+    </div>
+  )
+}
+
+export default SignIn
