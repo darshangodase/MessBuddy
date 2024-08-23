f19bfddbf9c9dde49a8669b560dc1b1ee8d4a564
darshangodase <darshangodase10@gmail.com>
2024-08-23 18:50:58 +0530

    mongodb cluster successfully connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch(err => console.log(err));
 
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
-  });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
