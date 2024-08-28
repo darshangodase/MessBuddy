0a754e19b9fdc287a9308639447eeab623eb2d06
darshangodase <darshangodase10@gmail.com>
2024-08-28 18:29:18 +0530

    created deleteuser api

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -60,4 +60,17 @@ const updateUser = async (req, res, next) => {
     }
 }
 
-module.exports = { test, updateUser };
+const deleteuser = async (req, res, next) => {
+      if(req.user.id != req.params.userId){
+        return next(errorHandler(400, "You are not authorized to delete this user"));
+      }
+      try{
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted');
+      }
+      catch(error)
+      {
+        res.status(200).json('Could not delete the user');
+      }
+}
+module.exports = { test, updateUser,deleteuser };
--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { test, updateUser } = require('../controllers/user_controller');
+const { test, updateUser,deleteuser } = require('../controllers/user_controller');
 const verifytoken = require('../utils/verifyuser');
 
 router.get('/test', test);
 router.put('/update/:userId', verifytoken, updateUser);
+router.delete('/delete/:userId',verifytoken, deleteuser)
 module.exports = router;
