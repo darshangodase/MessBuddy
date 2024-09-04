b86a15945722f9955abea5789fd4445f8e356219
darshangodase <darshangodase10@gmail.com>
2024-09-04 07:39:02 +0530

    fixes

--- backend/utils/verifyuser.js
+++ backend/utils/verifyuser.js
@@ -1,27 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = async (req, res, next) => {
+const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) {
     return res.status(401).json({ success: false, message: 'No token provided, authorization denied.' });
   }
 
-  try {
-    const decoded = await new Promise((resolve, reject) => {
-      jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(decoded);
-      });
-    });
-
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ success: false, message: 'Token is not valid.' });
+    }
     req.user = decoded;
     next();
-  } catch (err) {
-    return res.status(403).json({ success: false, message: 'Token is not valid.' });
-  }
+  });
 };
 
 module.exports = verifyToken;
